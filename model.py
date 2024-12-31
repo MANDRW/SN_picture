e2e9396baf481130be0a93b27720f2635a389e2a
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2024-12-31 02:29:59 +0100

    better model

--- model.py
+++ model.py
@@ -5,15 +5,16 @@ from tensorflow.keras.utils import to_categorical
 
 def cmodel(num_classes):
     model = Sequential([
-        Conv2D(64, (3, 3), activation='relu', input_shape=(224, 224, 3)),  # 32 filtry, rozmiar 3x3
-        Conv2D(64, (3, 3), activation='relu'),  # 32 filtry, rozmiar 3x3
+        Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+        Conv2D(64, (3, 3), activation='relu'),
         MaxPooling2D((2, 2)),  # Warstwa poolingowa
+        Dropout(0.25),
         Conv2D(128, (3, 3), activation='relu'),
         Conv2D(128, (3, 3), activation='relu'),# Druga warstwa konwolucyjna
         MaxPooling2D((2, 2)),
+        Dropout(0.25),
         Flatten(),  # Spłaszczenie cech
         Dense(128, activation='relu'),  # Warstwa w pełni połączona
-        Dropout(0.75),
          # Dropout, aby zapobiec przeuczeniu
         Dense(num_classes, activation='softmax')  # Warstwa wyjściowa z softmax dla klasyfikacji
     ])
