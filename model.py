2d6e50a40ca600752a4aa5e0ea260714eb0a837f
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-01-06 00:26:58 +0100

    small update

--- model.py
+++ model.py
@@ -7,16 +7,15 @@ def cmodel(num_classes):
     model = Sequential([
         Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
         Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),  # Warstwa poolingowa
-        Dropout(0.25),
-        Conv2D(128, (3, 3), activation='relu'),
-        Conv2D(128, (3, 3), activation='relu'),# Druga warstwa konwolucyjna
         MaxPooling2D((2, 2)),
         Dropout(0.25),
-        Flatten(),  # Spłaszczenie cech
-        Dense(128, activation='relu'),  # Warstwa w pełni połączona
-         # Dropout, aby zapobiec przeuczeniu
-        Dense(num_classes, activation='softmax')  # Warstwa wyjściowa z softmax dla klasyfikacji
+        Conv2D(128, (3, 3), activation='relu'),
+        Conv2D(128, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Dropout(0.25),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(num_classes, activation='softmax')
     ])
 
     optimazer = Adam(learning_rate=0.0001)
